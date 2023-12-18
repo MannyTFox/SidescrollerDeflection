c84dbd3725597ab6fca6b1c1955249bbcc60351c
MannyTFox <59266181+MannyTFox@users.noreply.github.com>
2023-12-18 03:32:01 -0300

    Air attacks and some animation fixes

--- Scenes/Characters/Player/Player.gd
+++ Scenes/Characters/Player/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 @export var characterHorizontalMovement : CharacterHorizontalMovement 
 @export var characterGravity : CharacterGravity
 @export var characterJump : CharacterJump
+var canAttack = true
 var comboCounter = 0
 
  #this method has to be here since animation events only work when calling something on a parent, it simply does not call for brothers or cousins in the hierarchy
@@ -16,14 +17,19 @@ func _reset_combo_counter():
 func _increase_combo_counter():
 	comboCounter += 1
 	$ComboTimeLimit.start()
-	
+func _start_combo_cooldown():
+	canAttack = false
+	$ComboCooldown.start()
+func _on_combo_cooldown_timeout():
+	canAttack = true
+
 func _on_combo_time_limit_timeout():
 	if comboCounter != 0:
 		comboCounter = 0
 		
 
 func _physics_process(delta):
-	
+
 	$FunctionLabel.text = str(comboCounter)
 	
 	
@@ -32,13 +38,12 @@ func _physics_process(delta):
 		if Input.is_action_pressed("Guard"):
 			characterHorizontalMovement._move(0)
 			$AnimationTree.get("parameters/playback").travel("Guard")
-		
-		elif Input.is_action_just_pressed("Attack"):
+			
+		elif Input.is_action_just_pressed("Attack") && canAttack:
 			if comboCounter <= 0:
 				$AnimationTree.get("parameters/playback").travel("Attack1")
 			else:				
-				$AnimationTree.get("parameters/playback").travel("Attack2")
-				
+				$AnimationTree.get("parameters/playback").travel("Attack2")		
 			
 		elif Input.is_action_just_pressed("Jump"):
 			characterJump._jump()
@@ -51,10 +56,16 @@ func _physics_process(delta):
 		elif is_zero_approx(Input.get_axis("Left", "Right")):
 			characterHorizontalMovement._move(0)
 			$AnimationTree.get("parameters/playback").travel("Idle")
-	else:
+	else:		
 		characterGravity._apply_gravity(delta)
 		characterHorizontalMovement._move(Input.get_axis("Left", "Right"))
-		$AnimationTree.get("parameters/playback").travel("Fall")
+		if Input.is_action_just_pressed("Attack") && canAttack:
+			if comboCounter <= 0:
+				$AnimationTree.get("parameters/playback").travel("Attack1")
+			else:				
+				$AnimationTree.get("parameters/playback").travel("Attack2")
+		else:
+			$AnimationTree.get("parameters/playback").travel("Fall")
 		
 
 	if Input.get_axis("Left", "Right") > 0 && !characterHorizontalMovement.frozen:
@@ -63,6 +74,8 @@ func _physics_process(delta):
 		$Sprite.flip_h = true
 		
 	move_and_slide()
+
+
 
 
 
